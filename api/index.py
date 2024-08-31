3d44ac47990705e2b97672e598b63368d2113c35
joel-w-davis50 <98930173+joel-w-davis50@users.noreply.github.com>
2024-08-31 15:24:24 -0400

    push 4

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ from openai import OpenAI
 
 
 # Load environment variables from the .env file
-load_dotenv(find_dotenv())
+load_dotenv()
 OPENAI_API_KEY= os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
